46b733e75f0c22e351edcc7813fc515ccc353f27
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2022-12-01 17:19:46 +0000

    Add navbar to rails app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root 'pages#hello'
+  # root 'pages#hello'
+  # GET /about
+  get "about", to: "about#index"
+  root  "main#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
