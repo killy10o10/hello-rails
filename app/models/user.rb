102707b3f6f4b31d3bc3bf34ad532fe6833cd325
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2022-12-08 18:05:22 +0000

    Add associations to models
    create CRUD methods to alter database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_many :likes, foreign_key: 'author_id'
+  has_many :posts, foreign_key:  'author_id'
+  has_many :comments, foreign_key: 'author_id'
+
+  def select_top_three
+    posts.order(created_at: :asc).limit(3)
+  end
 end
